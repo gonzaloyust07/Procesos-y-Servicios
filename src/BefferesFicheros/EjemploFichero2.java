80697840e611141a8cc942f3b92d2da34bcc5437
gonzaloyust07 <gonzaloyustemiras@gmail.com>
2026-09-22 18:49:17 +0200

    22/09/26-1

---
+++ src/BefferesFicheros/EjemploFichero2.java
@@ -0,0 +1,19 @@
+package BefferesFicheros;
+
+import java.io.BufferedWriter;
+
+public class EjemploFichero2 {
+    public static void main(String[]args){
+
+        //Ejemplo de escritura de un fichero de texto
+        //Escribe 3 o 4 palabras de un fichero
+
+        BufferedWriter
+
+
+        /*Mejora: Seras capaz de pedir esas palabras por teclado
+        y escribirlas en un fichero??*/
+        //Pararias de pedir palabras cuando la palabra introducida sea 'adios'
+
+    }
+}
